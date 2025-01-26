e9ac68f0d812454713db39a14f5071f0f93c1431
Def4ultUs3r <middle.it.developer@gmail.com>
2025-01-27 00:22:43 +0200

    Messages rebuilding

--- Messages.cs
+++ Messages.cs
@@ -1,14 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Telegram.Bot.Types.ReplyMarkups;
+﻿using Telegram.Bot.Types.ReplyMarkups;
 using Telegram.Bot;
-using Telegram.Bots.Extensions.Polling;
 using Telegram.Bot.Polling;
 using Telegram.Bot.Types;
-using Newtonsoft.Json.Linq;
+using System.Diagnostics.Metrics;
+//using Telegram.Bots.Types;
+//using Telegram.Bots.Types;
+//using Telegram.Bots.Types;
+//using Telegram.Bots.Types;
 
 namespace Telegram_bot
 {
@@ -37,12 +35,14 @@ namespace Telegram_bot
             );
         }
 
-        public static async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
+        public static async Task HandleUpdateAsync(ITelegramBotClient botClient, Telegram.Bot.Types.Update update, CancellationToken cancellationToken)
         {
 
             #region MainCode
-            // Некоторые действия
+            //  Всі дії
             Console.WriteLine(Newtonsoft.Json.JsonConvert.SerializeObject(update));
+
+
             if (update.Type == Telegram.Bot.Types.Enums.UpdateType.Message)
             {
                 var message = update.Message;
@@ -56,21 +56,62 @@ namespace Telegram_bot
             if (update.Type == Telegram.Bot.Types.Enums.UpdateType.CallbackQuery)
             {
                 string codeOfButton = update.CallbackQuery.Data;
-                if (codeOfButton == "post")
-                {
-                    Console.WriteLine("You press button 1");
-                    string telegramMessage = "You press button 1";
-                    await botClient.SendMessage(chatId: update.CallbackQuery.Message.Chat.Id, telegramMessage, parseMode: Telegram.Bot.Types.Enums.ParseMode.Html);
-                }
-                if (codeOfButton == "12")
-                {
-                    Console.WriteLine("You press button 2");
-                    string telegramMessage = "Виберіть спеціальність:";
-                    // await botClient.SendTextMessageAsync(chatId: update.CallbackQuery.Message.Chat.Id, telegramMessage, parseMode: Telegram.Bot.Types.Enums.ParseMode.Html);
 
-                    InlineKeyboardMarkup inlineKeyBoard = new InlineKeyboardMarkup(
-                        new[]
+                switch (codeOfButton)
+                {
+                    case "training":
                         {
+                            Console.WriteLine("training");
+                            string telegramMessage = "Підготовчі курси:\n";
+
+                            var filePath = Path.Combine("Resources", "training.jpg");
+
+                            using (var stream = new FileStream(filePath, FileMode.Open, FileAccess.Read))
+                            {
+                                await botClient.SendPhoto(
+                                    chatId: update.CallbackQuery.Message.Chat.Id,
+                                    photo: InputFile.FromStream(stream, "training.jpg"),
+                                    caption: telegramMessage,
+                                    cancellationToken: cancellationToken
+                                );
+                            }
+                        }
+                        break;
+
+                    case "main_menu":
+                        SendInline(botClient: botClient, chatId: update.CallbackQuery.Message.Chat.Id, cancellationToken: cancellationToken);
+                        break;
+
+                    case "map":
+                        {
+                            Console.WriteLine("show map");
+                            string telegramMessage = "Ми знаходимось за адресою:\nпровулок Ушинського, 3, Дніпро, Дніпропетровська область, 49000";
+                            await botClient.SendMessage(chatId: update.CallbackQuery.Message.Chat.Id, telegramMessage, parseMode: Telegram.Bot.Types.Enums.ParseMode.Html);
+
+                            double latitude = 48.461933773008624;  // Широта
+                            double longitude = 35.03352385563664; // Довгота
+
+                            await botClient.SendLocation(
+                                chatId: update.CallbackQuery.Message.Chat.Id,
+                                latitude: latitude,
+                                longitude: longitude,
+                                livePeriod: null, // Можна вказати livePeriod для відстеження
+                                cancellationToken: cancellationToken
+                            );
+
+                            ShowMainMenuButton(botClient: botClient, chatId: update.CallbackQuery.Message.Chat.Id, cancellationToken);
+                        }
+                        break;
+
+                    case "professions":
+                        {
+                            Console.WriteLine("You press button 2");
+                            string telegramMessage = "Виберіть спеціальність:";
+                            // await botClient.SendTextMessageAsync(chatId: update.CallbackQuery.Message.Chat.Id, telegramMessage, parseMode: Telegram.Bot.Types.Enums.ParseMode.Html);
+
+                            Telegram.Bot.Types.ReplyMarkups.InlineKeyboardMarkup inlineKeyBoard = new Telegram.Bot.Types.ReplyMarkups.InlineKeyboardMarkup(
+                                new[]
+                                {
                             // first row
                             new[]
                             {
@@ -80,11 +121,26 @@ namespace Telegram_bot
                                 InlineKeyboardButton.WithCallbackData(text: "Button 4", callbackData: "post4"),
                             },
 
-                        });
+                                });
 
-                    // await botClient.EditMessageCaptionAsync(chatId: update.CallbackQuery.Message.Chat.Id, caption: telegramMessage, messageId: update.CallbackQuery.Message.MessageId);
-                    await _bot.EditMessageText(update.CallbackQuery.Message.Chat.Id, update.CallbackQuery.Message.MessageId, telegramMessage, replyMarkup: inlineKeyBoard, parseMode: Telegram.Bot.Types.Enums.ParseMode.Html);
+                            
+                            await _bot.EditMessageText(update.CallbackQuery.Message.Chat.Id, update.CallbackQuery.Message.MessageId, telegramMessage, replyMarkup: inlineKeyBoard, parseMode: Telegram.Bot.Types.Enums.ParseMode.Html);
+                        }
+                        break;
+
+                    case "post3":
+                        SendInline(botClient: botClient, chatId: update.CallbackQuery.Message.Chat.Id, cancellationToken: cancellationToken);
+                        break;
+
+                    default:
+                        Console.WriteLine($"Default call");
+                        break;
                 }
+
+                
+                
+                
+                
             }
 
             #endregion
@@ -94,6 +150,28 @@ namespace Telegram_bot
         {
             // Некоторые действия
             Console.WriteLine(Newtonsoft.Json.JsonConvert.SerializeObject(exception));
+        }
+
+        public static async void ShowMainMenuButton(ITelegramBotClient botClient, long chatId, CancellationToken cancellationToken)
+        {
+            string telegramMessage = "Натисніть на кнопку👇, якщо бажаєте повернутись до головного меню\n";
+            InlineKeyboardMarkup inlineKeyboard = new InlineKeyboardMarkup(
+                // keyboard
+                new[]
+                {
+                     new[]
+                    {
+                        // first button in row
+                        InlineKeyboardButton.WithCallbackData(text: "Головне меню", callbackData: "main_menu")
+                        
+                    }
+                });
+
+            Message sentMessage = await botClient.SendMessage(
+                chatId: chatId,
+                text: telegramMessage,
+                replyMarkup: inlineKeyboard,
+                cancellationToken: cancellationToken);
         }
 
         public static async void SendInline(ITelegramBotClient botClient, long chatId, CancellationToken cancellationToken)
@@ -106,23 +184,47 @@ namespace Telegram_bot
                     new[]
                     {
                         // first button in row
-                        InlineKeyboardButton.WithCallbackData(text: "Події", callbackData: "post"),
+                        InlineKeyboardButton.WithCallbackData(text: "📚 Підготовчі курси", callbackData: "training"),
                         // second button in row
-                        InlineKeyboardButton.WithCallbackData(text: "Спеціальності", callbackData: "12"),
+                        InlineKeyboardButton.WithCallbackData(text: "🤝 День відкритих дверей", callbackData: "door")                        
                     },
                     // second row
                     new[]
                     {
+                        InlineKeyboardButton.WithCallbackData("📅 Графік навчання", callbackData: "plan"),
+                        InlineKeyboardButton.WithCallbackData("🕰️ Розклад дзвінків", callbackData: "time")                        
+
+                    },
+                    //third row
+                    new[]
+                    {                        
+                        InlineKeyboardButton.WithCallbackData("📖 Розклад занять(II семестр)", callbackData: "schedule")
+
+                    },
+                    
+                    new[]
+                    {
+                        InlineKeyboardButton.WithCallbackData(text: "🧑‍🎓 Спеціальності", callbackData: "professions"),
+                        InlineKeyboardButton.WithCallbackData("🪙 Платні послуги", callbackData: "money")
+
+                    },
+                    new[]
+                    {
                         // first button in row
-                        InlineKeyboardButton.WithUrl(text: "Сайт коледжу", url: "https://www.dktd.dp.ua/"),
-                        InlineKeyboardButton.WithCallbackData("Залишити контактні дані")
+                        InlineKeyboardButton.WithUrl(text: "🌐 Сайт коледжу", url: "https://www.dktd.dp.ua/"),
+                        InlineKeyboardButton.WithCallbackData("📇 Контакти", callbackData: "contacts")
+                    },
+                    new[]
+                    {
+                        InlineKeyboardButton.WithCallbackData(text: "🧭 Як нас знайти", callbackData: "map")
+                        
                     },
 
                 });
 
             Message sentMessage = await botClient.SendMessage(
                 chatId: chatId,
-                text: "Головне меню\nВиберіть пункт, який Вас зацікавив:\n ",
+                text: "Головне меню\n\nВиберіть пункт, який Вас зацікавив:\n ",
                 replyMarkup: inlineKeyboard,
                 cancellationToken: cancellationToken);
         }
--- obj/Debug/net8.0/Telegram bot.AssemblyInfo.cs
+++ obj/Debug/net8.0/Telegram bot.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Telegram bot")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e174d3c87287500c3f64f752765c44b5f68dbf01")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d07eaa01805831f36993028d142452cdf34a58bf")]
 [assembly: System.Reflection.AssemblyProductAttribute("Telegram bot")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Telegram bot")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
